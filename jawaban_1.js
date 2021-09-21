6fe81cbd279449c46479aad7c396af4397b28112
rezaagust <reza.2020@smk-strada.sch.id>
2021-09-21 09:15:00 +0700

    tugas reza

--- jawaban_1.js
+++ jawaban_1.js
@@ -1,1 +1,26 @@
 // Jawaban Soal 1 ada di bawah ini
+class Hewan {
+    constructor(nama, warna, beratGram, harga) {
+        this.nama = nama;
+        this.warna = warna;
+        this.beratGram = beratGram;
+        this.harga = harga;
+        this.kondisiBagus = false;
+    }
+
+    cekKondisiBagus(kondisi = false) {
+        return this.kondisiBagus = kondisi;
+    }
+}
+
+// Cara membuat object instance
+
+let hewanKuda = new Hewan("Kuda", "Cokelat", 1000, 1000000);
+hewanKuda.cekKondisiBagus(true);
+console.log(hewanKuda);
+let hewanBurung = new Hewan("Burung", "Putih", 25, 100000);
+hewanBurung.cekKondisiBagus();
+console.log(hewanBurung);
+
+console.log('ini hewan kuda =>', hewanKuda);
+console.log('ini hewan burung =>', hewanBurung);
